2f9fb62451a45897b3240923b0c006474c20b81b
Jasper N. Brouwer <jasper@nerdsweide.nl>
2015-03-13 11:22:57 +0100

    Added the serializer

---
+++ src/Serializer/Serializer.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @license https://github.com/simple-es/jms-serializer-bridge/blob/master/LICENSE MIT
+ */
+
+namespace SimpleES\JMSSerializerBridge\Serializer;
+
+use JMS\Serializer\SerializerInterface as JMSSerializer;
+use SimpleES\EventSourcing\Event\Resolver\ResolvesEventNames;
+use SimpleES\EventSourcing\Serializer\SerializesData;
+
+/**
+ * @copyright Copyright (c) 2015 Future500 B.V.
+ * @author    Jasper N. Brouwer <jasper@future500.nl>
+ */
+class Serializer implements SerializesData
+{
+    /**
+     * @var JMSSerializer
+     */
+    private $jmsSerializer;
+
+    /**
+     * @var string
+     */
+    private $format;
+
+    /**
+     * @param JMSSerializer $jmsSerializer
+     * @param string        $format
+     */
+    public function __construct(JMSSerializer $jmsSerializer, $format)
+    {
+        $this->jmsSerializer = $jmsSerializer;
+        $this->format        = $format;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function serialize($data)
+    {
+        return $this->jmsSerializer->serialize($data, $this->format);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deserialize($data, $type)
+    {
+        return $this->jmsSerializer->deserialize($data, $type, $this->format);
+    }
+}
---
+++ tests/SerializerTest.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * @license https://github.com/simple-es/jms-serializer-bridge/blob/master/LICENSE MIT
+ */
+
+namespace SimpleES\JMSSerializerBridge\Test;
+
+use SimpleES\JMSSerializerBridge\Serializer\Serializer;
+
+/**
+ * @copyright Copyright (c) 2015 Future500 B.V.
+ * @author    Jasper N. Brouwer <jasper@future500.nl>
+ */
+class SerializerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Serializer
+     */
+    private $serializer;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $jmsSerializer;
+
+    public function setUp()
+    {
+        $this->jmsSerializer = $this->getMock('JMS\Serializer\SerializerInterface');
+
+        $this->serializer = new Serializer($this->jmsSerializer, 'json');
+    }
+
+    public function tearDown()
+    {
+        $this->serializer    = null;
+        $this->jmsSerializer = null;
+    }
+
+    /**
+     * @test
+     */
+    public function itUsesJMSSerializerToSerializeData()
+    {
+        $object = new \stdClass();
+        $string = '{"some_key": "Some value"}';
+
+        $this->jmsSerializer
+            ->expects($this->once())
+            ->method('serialize')
+            ->with($object, 'json')
+            ->will($this->returnValue($string));
+
+        $serializedObject = $this->serializer->serialize($object);
+
+        $this->assertSame($string, $serializedObject);
+    }
+
+    /**
+     * @test
+     */
+    public function itUsesJMSSerializerToDeserializeData()
+    {
+        $string = '{"some_key": "Some value"}';
+        $object = new \stdClass();
+
+        $this->jmsSerializer
+            ->expects($this->once())
+            ->method('deserialize')
+            ->with($string, 'stdClass')
+            ->will($this->returnValue($object));
+
+        $deserializedObject = $this->serializer->deserialize($string, 'stdClass');
+
+        $this->assertSame($object, $deserializedObject);
+    }
+}
